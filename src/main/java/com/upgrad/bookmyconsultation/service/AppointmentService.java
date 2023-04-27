30d284674b9616721fb3e2e209eb39cf6fffb839
Anuj Chopra <anujchopra100@gmail.com>
2023-04-27 15:16:22 +0530

    first commit

---
+++ src/main/java/com/upgrad/bookmyconsultation/service/AppointmentService.java
@@ -0,0 +1,51 @@
+package com.upgrad.bookmyconsultation.service;
+
+import com.upgrad.bookmyconsultation.entity.Appointment;
+import com.upgrad.bookmyconsultation.exception.InvalidInputException;
+import com.upgrad.bookmyconsultation.exception.ResourceUnAvailableException;
+import com.upgrad.bookmyconsultation.exception.SlotUnavailableException;
+import com.upgrad.bookmyconsultation.repository.AppointmentRepository;
+import com.upgrad.bookmyconsultation.repository.UserRepository;
+import com.upgrad.bookmyconsultation.util.ValidationUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AppointmentService {
+
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    public String appointment(Appointment appointment) throws SlotUnavailableException, InvalidInputException {
+        ValidationUtils.validate(appointment);
+        Appointment existingAppointment = appointmentRepository.findByDoctorIdAndTimeSlotAndAppointmentDate(appointment.getDoctorId(), appointment.getTimeSlot(), appointment.getAppointmentDate());
+
+        if (existingAppointment != null) {
+            throw new SlotUnavailableException();
+        }
+        appointmentRepository.save(appointment);
+        return appointment.getAppointmentId();
+    }
+
+
+    public Appointment getAppointment(String appointmentId) {
+        return Optional.ofNullable(appointmentRepository.findById(appointmentId))
+                .get()
+                .orElseThrow(ResourceUnAvailableException::new);
+    }
+
+    public List<Appointment> getAppointmentsForUser(String userId) {
+        return appointmentRepository.findByUserId(userId);
+    }
+}
---
+++ src/main/java/com/upgrad/bookmyconsultation/service/RatingsService.java
@@ -0,0 +1,42 @@
+package com.upgrad.bookmyconsultation.service;
+
+import com.upgrad.bookmyconsultation.entity.Doctor;
+import com.upgrad.bookmyconsultation.entity.Rating;
+import com.upgrad.bookmyconsultation.repository.DoctorRepository;
+import com.upgrad.bookmyconsultation.repository.RatingsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+
+@Service
+public class RatingsService {
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @Autowired
+    private RatingsRepository ratingsRepository;
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+
+    public void submitRatings(Rating rating) {
+        rating.setId(UUID.randomUUID().toString());
+        ratingsRepository.save(rating);
+
+        Double avgRating = ratingsRepository
+                .findByDoctorId(rating.getDoctorId())
+                .stream()
+                .collect(Collectors.averagingInt(Rating::getRating));
+        Doctor doctor = doctorRepository.findById(rating.getDoctorId()).get();
+        doctor.setRating(avgRating);
+        doctorRepository.save(doctor);
+
+
+    }
+}
